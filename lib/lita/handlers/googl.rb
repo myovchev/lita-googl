e8a6e8aea29a71db3fb001a59ccfd8d97af33826
Miroslav Yovchev <m.yovchev@corllete.com>
2015-05-17 00:32:53 +0300

    Removing expand, awaiting future OAuth (service) implementation

--- lib/lita/handlers/googl.rb
+++ lib/lita/handlers/googl.rb
@@ -11,26 +11,18 @@ module Lita
       config :api_key, type: String, required: true
       config :ip, required: false, default: nil
 
-      route %r{^googl\s+(:?expand)?\s*(.\S+)$}i, :googl, command: true, help: {
+      route %r{^googl\s+(.\S+)$}i, :googl, command: true, help: {
         'googl' => 'Shorten original or expand shortened URL.'
       }
 
       def googl(response)
-        expand = response.matches[0][0] == 'expand' || false
-        url = response.matches[0][1]
+        url = response.matches[0][0]
         raise GooglError if url.nil?
 
         api_key = Lita.config.handlers.googl.api_key
         ip = Lita.config.handlers.googl.ip
         log.debug("Googl IP: #{ip}")
         log.debug("Googl Input URL -  #{url}")
-
-        if expand
-          result = ::Googl.expand(url)
-          return response.reply(
-            "#{response.user.mention_name} #{result.long_url}"
-          )
-        end
 
         result = ::Googl.shorten(url, ip, api_key)
         response.reply("#{response.user.mention_name} #{result.short_url}")
--- spec/lita/handlers/googl_spec.rb
+++ spec/lita/handlers/googl_spec.rb
@@ -3,7 +3,6 @@ require "spec_helper"
 describe Lita::Handlers::Googl, lita_handler: true do
 
   it { is_expected.to route_command("googl http://google.com").to(:googl) }
-  it { is_expected.to route_command("googl expand http://google.com").to(:googl) }
 
   it "should do nothing on invalid request" do
     send_command("googl ")
@@ -40,20 +39,5 @@ describe Lita::Handlers::Googl, lita_handler: true do
     user1 = Lita::User.create(123, name: "user1", mention_name: 'user1')
     send_command("googl #{long_url}", as: user1)
     expect(replies.last).to eq("#{user1.name} #{short_url}")
-  end
-
-  it "should send expand request" do
-    long_url = 'http://zaralab.org/'
-    short_url = 'http://goo.gl/x5PsTf'
-
-    expand = ::Googl::Expand.new
-    allow(expand).to receive(:short_url).and_return(short_url)
-    allow(expand).to receive(:long_url).and_return(long_url)
-
-    allow(::Googl).to receive(:expand).with(short_url).and_return(expand)
-
-    user1 = Lita::User.create(123, name: "user1", mention_name: 'user1')
-    send_command("googl expand #{short_url}", as: user1)
-    expect(replies.last).to eq("#{user1.name} #{long_url}")
   end
 end
